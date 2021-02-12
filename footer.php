93a05b2e48f56a59203a85513f3a858fcbfbb00d
aliza-ansari <73295005+aliza-ansari@users.noreply.github.com>
2021-02-12 14:18:34 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,52 @@
+
+	
+    <!-- same Footer -->
+    <!-- Footer -->
+    <footer  class="page-footer font-small purple pt-4 relative-bottom bg-dark">
+
+        <!-- Footer Links -->
+        <div class="main-content">
+		        <div class="left box">
+              <h2 class="text-uppercase font-bold fheading">About Us</h2> 
+			  <hr class="hr1 ml-0" style="width:200px; background-color:white "></hr>
+		        <div class="content">
+ 
+                    <p class=" text-light">Specs & shades provides you platform where you can check out new designs of specs on your face virtually</p>
+
+		       
+</div>
+</div>
+
+		        <div class="center box">
+			 
+				<div class="content">
+								 
+                    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="#"> Specs & Shades</a>
+        </div> 
+		
+								   </div>
+								   </div>
+								   
+								   
+								  
+		
+		
+								  <div class="right box"> 
+								  <h2   class="text-uppercase font-bold fheading" > Social Links</h2>
+								  <hr class="hr1 ml-0" style="width:200px; background-color:white "></hr>
+								  <div class="content">
+								   <div class="social">
+								   
+<a href="#"><span class="fab fa-facebook-f"></span></a>
+<a href="#"><span class="fab fa-twitter"></span></a>
+<a href="#"><span class="fab fa-instagram"></span></a>
+ 
+</div>
+								   					   
+</div>
+    </footer>
+    <!-- Footer -->
+	
+	   
+	
