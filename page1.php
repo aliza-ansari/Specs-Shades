c80c64dd0fb7cb6ddcaf6e54f0557e60af8edbdb
aliza-ansari <73295005+aliza-ansari@users.noreply.github.com>
2021-02-12 14:16:41 +0500

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,143 @@
+ <?php include("header.php"); ?> 
+ 
+ 
+ 
+  
+<!-- !PAGE CONTENT! -->
+<!-- !different CONTENT of page 1! -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+   .carousel-inner img {
+    width: 250px;
+    height: 250px;
+  }
+  </style>
+</head>
+<body>
+<div id= "wholepage">
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass8.jpg"  width="100px" height="50px">
+    </div>
+    <div class="carousel-item">
+      <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass6.jpg" width="100px" height="50px">
+    </div>
+    <div class="carousel-item">
+      <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass5.jpg"   width="100px" height="50px">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+
+
+
+
+
+  <br></br>
+  
+  <div class="galleria"> 
+
+<div class="gallery"> 
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass1.jpg" width="60" height="60">
+ </div>
+
+ 
+<div class="gallery">
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\g2.jpeg" width="60" height="60">
+ </div>
+ 
+ <div class="gallery"> 
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass3.jpg" width="60" height="60">
+  </div>
+
+  
+<div class="gallery"> 
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass4.jpg" width="60" height="60">
+ </div>
+ 
+ 
+<div class="gallery"> 
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass5.jpg" width="60" height="60">
+  </div>
+
+  
+<div class="gallery"> 
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass6.jpg" width="60" height="60">
+ </div>
+
+ 
+<div class="gallery">
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass7.jpg" width="60" height="60">
+ </div>
+ 
+ <div class="gallery"> 
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass8.jpg" width="60" height="60">
+  </div>
+
+  
+<div class="gallery"> 
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass9.jpg" width="60" height="60">
+ </div>
+
+<br></br>
+  
+<div class="gallery"> 
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass6.jpg" width="60" height="60">
+ </div>
+
+ 
+<div class="gallery">
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass7.jpg" width="60" height="60">
+ </div>
+ 
+ <div class="gallery"> 
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass8.jpg" width="60" height="60">
+  </div>
+
+  
+<div class="gallery"> 
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass9.jpg" width="60" height="60">
+ </div>
+ 
+<div class="gallery">
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass10.jpg" width="60" height="60">
+ </div>
+ 
+ 
+<div class="gallery">
+    <img src="C:\Users\Aliza Ansari\Desktop\glasses_images\glass11.jpg" width="60" height="60">
+ </div>
+ 
+</div>
+
+
+  
+   
+<!-- End page content -->
+  
+ 
+ 
+ <?php include("footer.php"); ?> 
+ 
